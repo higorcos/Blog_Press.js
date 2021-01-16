7d56de638759b2cac548407bde006e2271524d64
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2021-01-16 00:07:07 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
 const connection = require('./database/database')
+const routerCategories = require('./categories/categoriesController') // rotas em outro arquivo 
+const routerArticles = require('./articles/articlesController')
 
 //view engine
 app.set('view engine','ejs');//para usar o ejs para carregar as páginas
@@ -13,14 +15,20 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json())
 
+//database
 //connection (conexão com bando de dados)
 connection
-    .authenticate()
-    .then(()=> {
-        console.log('conexão com banco de dados foi um sucesso!')
-    }).catch((error)=>{
-        console.log(error)
-    })
+.authenticate()
+.then(()=> {
+    console.log('conexão com banco de dados foi um sucesso!')
+}).catch((error)=>{
+    console.log(error)
+})
+
+//router (rotas em outro arquivo)
+app.use('/', routerCategories);
+app.use('/', routerArticles);
+
 
 app.get('/', (req,res) =>{
  res.render('index');
