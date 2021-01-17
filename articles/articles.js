da38aac20fea1f1344197dbf05bec64f2c887eba
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2021-01-17 11:57:10 -0300

    Add files via upload

--- articles/articles.js
+++ articles/articles.js
@@ -1,13 +1,25 @@
-const Sequelize = require("sequelize")
-const connection = require("../database/database")
+const Sequelize = require('sequelize');
+const connection = require('../database/database')
+const category = require('../categories/category')
 
-const category = connection.define('categories',{
+
+const article = connection.define('articles',{
     title:{
-        type: Sequelize.toString,
+        type: Sequelize.STRING,
         allowNull: false
-    },slug:{ //url exetempo 'dados do site' slug disso 'dados-do-site'
-        type: Sequelize.toString,
+    },slug: { //url exetempo 'dados do site' slug disso 'dados-do-site'
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    body: {
+        type: Sequelize.TEXT,
         allowNull: false
     }
 })
-module.exports = category;
+//article.sync({force: true})// vai forçar a criação da tabela
+
+article.belongsTo(category); //relacinamento 1 para 1 com siquelize
+category.hasMany(article); //relacinamento 1 para M(muitos) com siquelize
+
+
+module.exports = article;
