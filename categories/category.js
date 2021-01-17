0ae1fdcb6c10462b76e5c08122486f6dd04474e8
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2021-01-17 11:57:36 -0300

    Add files via upload

--- categories/category.js
+++ categories/category.js
@@ -1,14 +1,17 @@
-const { Sequelize } = require("sequelize")
-const Sequelize = require("sequelize")
-const connection = require("../database/database")
+const Sequelize = require('sequelize')
+const connection = require('../database/database')
+
 
 const category = connection.define('categories',{
     title:{
-        type: Sequelize.toString,
+        type: Sequelize.STRING,
         allowNull: false
-    },slug:{ //url exetempo 'dados do site' slug disso 'dados-do-site'
-        type: Sequelize.toString,
+    },slug: { //url exetempo 'dados do site' slug disso 'dados-do-site'
+        type: Sequelize.STRING,
         allowNull: false
     }
 })
+//category.sync({force: true})// vai forçar a criação da tabela
+
+
 module.exports = category;
