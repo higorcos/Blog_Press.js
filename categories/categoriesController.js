5129edb628767d06ad7fa739e8e4cc1c93f7773e
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2021-01-19 01:33:05 -0300

    post(/delete)

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();//.Router para criar manipuladores de rota modulares e montáveis.
 const Category = require('./category')
 const slugify = require('slugify');// biblioteca para transformar texto em url 
+//const category = require('./category');
 
 //o router faz a dritribuição de rota fora do arquivo primario 
 router.get('/admin/categories/new',(req,res) =>{
@@ -23,6 +24,32 @@ router.post('/categories/save',(req,res) =>{
 
 });
 router.get("/admin/categories", (req,res) => {
-    res.render('admin/categories/index')
+
+    Category.findAll({raw: true}).then(category =>{
+    res.render('admin/categories/index',{categories: category });
+    });
+});
+
+router.post("/categories/delete", (req,res)=>{
+    var id_to_delete = req.body.id;
+    if(id_to_delete != undefined){//se não for indefined
+        if(!isNaN(id_to_delete)){//se for um número
+
+            Category.destroy({//deletar dados no banco de dados 
+                where: {
+                    id:id_to_delete
+                }
+                }).then(()=>{
+                    res.redirect('/admin/categories')
+                })
+
+        }else{//se não for um número
+        res.redirect('/admin/categories');
+        }
+    }else{// se for 
+        res.redirect('/admin/categories');
+    
+    }
 })
+
 module.exports = router;
