878909f416734b4ce81be1ac44185b0eada28fc6
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2021-01-19 22:18:31 -0300

    rotas para criação de artigo no blog

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const category = require('../categories/category');
 const router = express.Router();//.Router para criar manipuladores de rota modulares e montáveis.
+const Category = require('../categories/category')
 
 //o router faz a dritribuição de rota fora do arquivo primario 
 router.get('/articles',(req,res) =>{
     res.send('Rota dos artigos');
 })
 router.get('/admin/articles/new',(req,res) =>{
-    res.send('Rota para criar um novo artigo');
+    Category.findAll().then(categories => {
+        res.render('admin/articles/new', {categories: categories})
+    })
 })
 module.exports = router;
