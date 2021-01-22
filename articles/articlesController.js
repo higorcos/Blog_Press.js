c26a3be5e9023325476a1c0bf03d56474cadfe83
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2021-01-21 23:27:09 -0300

    rota para salvar novos posts

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -1,15 +1,34 @@
 const express = require('express');
-const category = require('../categories/category');
 const router = express.Router();//.Router para criar manipuladores de rota modulares e montáveis.
 const Category = require('../categories/category')
+const Article = require('../articles/articles')
+const slugify = require('slugify')
 
 //o router faz a dritribuição de rota fora do arquivo primario 
-router.get('/articles',(req,res) =>{
+router.get('/admin/articles',(req,res) =>{
     res.send('Rota dos artigos');
 })
 router.get('/admin/articles/new',(req,res) =>{
     Category.findAll().then(categories => {
         res.render('admin/articles/new', {categories: categories})
     })
+})
+router.post('/articles/save',(req,res) => {
+    let titleArticle = req.body.titleArticle
+    let textArticle = req.body.textArticle
+    let category = req.body.category
+
+    if(titleArticle != undefined && textArticle != undefined){
+        Article.create({
+            title:titleArticle ,
+            slug:slugify(titleArticle ),
+            body:textArticle,
+            categoryId: category
+        }).then(() => {
+            res.redirect('/admin/articles')
+        })
+    }else{
+        res.redirect('/')
+    }
 })
 module.exports = router;
