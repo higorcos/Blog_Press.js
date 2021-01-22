709c9435955231316ebd3aa95e4d50856aa0fda4
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2021-01-21 23:26:24 -0300

    time

--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 
 const connection = new Sequelize('Press.js','root','password',{
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '-03:00'//horario  para os horário do banco de dados
 
 });
 
