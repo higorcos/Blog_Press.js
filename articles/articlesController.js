15185b632a89235d02ea399eabdf7d5df5f3eb85
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2021-01-23 00:36:30 -0300

    Add files via upload

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -6,7 +6,9 @@ const slugify = require('slugify')
 
 //o router faz a dritribuição de rota fora do arquivo primario 
 router.get('/admin/articles',(req,res) =>{
-    Article.findAll().then(articles => {
+    Article.findAll({
+        include: [{model: Category}]
+    }).then(articles => {
         res.render('admin/articles/index',{articles:articles});
     })
 })
@@ -33,4 +35,57 @@ router.post('/articles/save',(req,res) => {
         res.redirect('/')
     }
 })
+
+router.post("/admin/articles/delete", (req,res)=> {
+
+    let id_to_delete = req.body.id;
+    if(id_to_delete != undefined){
+        if(!isNaN(id_to_delete)){
+            Article.destroy({
+                where:{
+                    id:id_to_delete
+                }
+            }).then(()=>{
+                res.redirect('/admin/articles');
+            })
+        }else{
+            res.redirect('/admin/articles')
+        }
+    }else{
+        res.redirect('/admin/articles')
+    }
+})
+
+router.get("/admin/articles/edit/:id", (req,res)=>{
+    let idSearch = req.params.id;
+    if(isNaN(idSearch)){
+        res.redirect('/admin/articles')
+    }else{
+        Article.findByPk(idSearch).then(article => {
+            if(article != undefined){
+                res.render('admin/articles/edit', {article: article})
+            }else{
+                res.redirect('/admin/articles') 
+            }
+        }).catch(erro => {
+             res.redirect('/admin/articles')
+        })
+    }
+})
+
+router.post('/admin/articles/update', (req,res) => {
+    let idUp = req.body.id;
+    let titleUp = req.body.title;
+    //let categoryUp = req.body.category
+
+    Article.uptade({ title: titleUp, slug: slugify(titleUp)},{
+        where: {
+            id:idUp
+        }
+    }).then(()=>{
+        res.redirect('/admin/articles')
+    })
+})
+
+
 module.exports = router;
