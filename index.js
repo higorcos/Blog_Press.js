a7fd8d873b2553c7bf24ea627645e421e3400f03
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2021-02-08 19:08:19 -0300

    rotas pagina principal

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const routerArticles = require('./articles/articlesController')
 
 const Article = require('./articles/articles');
 const Category = require('./categories/category');
+const article = require('./articles/articles');
 
 //view engine
 app.set('view engine','ejs');//para usar o ejs para carregar as páginas
@@ -35,8 +36,29 @@ app.use('/', routerArticles);
 
 
 app.get('/', (req,res) =>{
- res.render('index');
+    Article.findAll({order: [['id','DESC']]}).then(articles =>{
+        res.render('index',{articles: articles});
+
+    });
 })
+
+app.get('/:slug', (req,res)=> {
+    let slugSearch = req.params.slug;
+    Article.findOne({
+        where:{
+            slug:slugSearch
+        
+        }
+    }).then(article=>{
+        if(article != undefined){
+            res.render('article',{article: article});
+        }else{
+            res.redirect('/');
+        }
+    }).catch(err => {
+        res.redirect('/')
+    })
+});
 
 app.listen(8080,()=>{
     console.log('..........Servidor online');
