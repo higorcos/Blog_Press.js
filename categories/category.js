d8d74773968be37f37ea18b2fc9403491eb23c15
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2021-02-24 21:00:16 -0300

    Backup

--- categories/category.js
+++ categories/category.js
@@ -11,7 +11,6 @@ const category = connection.define('categories',{
         allowNull: false
     }
 })
+
 //category.sync({force: true})// vai forçar a criação da tabela
-
-
 module.exports = category;
