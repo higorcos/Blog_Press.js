73f9a1d7a991022b92f41815cf2153d59e1d0a2c
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2021-02-24 21:01:49 -0300

    .

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const routerArticles = require('./articles/articlesController')
 const Article = require('./articles/articles');
 const Category = require('./categories/category');
 const article = require('./articles/articles');
+const category = require('./categories/category');
 
 //view engine
 app.set('view engine','ejs');//para usar o ejs para carregar as páginas
@@ -36,9 +37,12 @@ app.use('/', routerArticles);
 
 
 app.get('/', (req,res) =>{
-    Article.findAll({order: [['id','DESC']]}).then(articles =>{
-        res.render('index',{articles: articles});
-
+    Article.findAll(
+        {order: [['id','DESC']]
+    }).then(articles =>{
+        Category.findAll().then(categories =>{
+            res.render('index',{articles: articles, categories:categories});
+             })
     });
 })
 
@@ -47,11 +51,12 @@ app.get('/:slug', (req,res)=> {
     Article.findOne({
         where:{
             slug:slugSearch
-        
         }
-    }).then(article=>{
+    }).then(article =>{
         if(article != undefined){
-            res.render('article',{article: article});
+            Category.findAll().then(categories =>{
+                res.render('article', {article: article, categories:categories});
+                 })
         }else{
             res.redirect('/');
         }
@@ -59,6 +64,27 @@ app.get('/:slug', (req,res)=> {
         res.redirect('/')
     })
 });
+
+app.get('/category/:slug', (req,res) => { //rota para flitrar as caregorias
+    let slug = req.params.slug;
+    Category.findOne({
+        where:{
+            slug:slug
+        },
+        include:[{model: Article}]
+    }).then(category => {
+
+        if (category != undefined){
+           Category.findAll().then(categories => {
+               res.render('index', {articles: category.articles, categories: categories})//só é possivel passar achar o tabela do artigo se puxar pela tabela category
+           })
+        } else {
+            res.redirect("/");
+        }
+    }).catch(err => {
+        res.redirect("/");
+    })
+})
 
 app.listen(8080,()=>{
     console.log('..........Servidor online');
