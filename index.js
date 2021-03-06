dec37d4349494f9c57df77a6d200bff681785b28
higor costa <higorcosta2001@outlook.com>
2021-03-06 20:45:29 -0300

    Base para sistema de login

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const connection = require('./database/database')
 
 const routerCategories = require('./categories/categoriesController') // rotas em outro arquivo 
 const routerArticles = require('./articles/articlesController')
+const routerUsers = require('./user/userControler')
 
 const Article = require('./articles/articles');
 const Category = require('./categories/category');
+const User = require('./user/user')
+
 const article = require('./articles/articles');
 const category = require('./categories/category');
-const router = require('./categories/categoriesController');
+const router = require('./categories/categoriesController'); 
 
 //view engine
 app.set('view engine','ejs');//para usar o ejs para carregar as páginas
@@ -35,7 +38,7 @@ connection
 //router (rotas em outro arquivo)
 app.use('/', routerCategories);
 app.use('/', routerArticles);
-
+app.use('/', routerUsers)
 
 app.get('/', (req,res) =>{
     Article.findAll(
---
+++ user/user.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize')
+const connection = require('../database/database')
+
+
+const user = connection.define('users',{
+    email:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },password: { //url exetempo 'dados do site' slug disso 'dados-do-site'
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+})
+//user.sync({force: false})// Vai criar a tabela quando ela ainda não existir 
+//user.sync({force: true})// vai forçar a criação da tabela
+module.exports = user;
