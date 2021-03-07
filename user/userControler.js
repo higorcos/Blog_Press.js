a8aacd17b9f5446b3274cdf0813727a7bda5cee2
higor costa <higorcosta2001@outlook.com>
2021-03-06 23:03:54 -0300

    Validação de email

--- user/userControler.js
+++ user/userControler.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require('./user');
+const bcrypt = require('bcryptjs');//biblioteca para fazer o hash de senha 
 
 
 router.get('/admin/users', (req,res)=> {
@@ -14,7 +15,26 @@ router.post('/user/save', (req,res) => {
     let email = req.body.email;
     let password = req.body.password;
 
-    res.json({email, password})
+    User.findOne({
+        where: {email: email}
+    }).then(user =>{
+        if(user == undefined){
+        var salt = bcrypt.genSaltSync(10); // uma segurança extra   
+        var hash = bcrypt.hashSync(password, salt);
+    
+        User.create({
+            email: email,
+            password: hash
+        }).then(() =>{
+            res.json({email, hash})
+        }).catch((err)=>{
+           res.redirect('/')
+        })
+    }else{
+        res.redirect('/user/create')
+    }
+    })
+
 })
 
 module.exports = router;
